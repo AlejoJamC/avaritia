68ef2271c88b67ca12d21cd27a7838b1032c6818
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-08-21 21:08:58 -0500

    Routes updated

--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,56 @@
 /**
- * Created by alejopro on 21/08/15.
+ * Module dependencies
  */
+var Logger = require('../config/logger');
+var logger = Logger.logger;
+var moment = require('moment');
+
+/**
+ * setupRouter
+ *
+ * @description Configure all routes on express router
+ *
+ * @param {express.Router}      router      The varaible router used by the server
+ */
+function setupRouter (router){
+
+    // logger for all request will first hits this middleware
+    router.use(function (req, res, next) {
+        var now = moment(new Date());
+
+        var date = now.format("DD-MM-YYYY HH:mm");
+        logger.info('%s %s %s', req.method, req.url, date);
+        next();
+    });
+
+    /**
+     *  Declare all routes
+     */
+    var userRoutes = require('./users');
+
+    /**
+     *  Document:  USERS.JS
+     *  Define routes where they are stored endpoints
+     */
+    //// ENDPOINT: /users
+    //router.route('/users')
+    //    .get(userRoutes.getUsers)
+    //    .post(userRoutes.postUser);
+    //
+    //// ENDPOINT: /users/:id
+    //router.route('/users/:id')
+    //    .get(userRoutes.getUserById)
+    //    .put(userRoutes.putUser)
+    //    .patch(userRoutes.patchUser)
+    //    .delete(userRoutes.deleteUser);
+    //
+    //// ENDPOINT: /login
+    //router.route('/login')
+    //    .get(userRoutes.getLogin);
+    /**
+     * ====================================================================
+     */
+}
+
+// Export the function that initialize all routes
+module.exports.setupRouter = setupRouter;
