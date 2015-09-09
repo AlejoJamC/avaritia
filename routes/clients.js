8c84ad1b75d3d5585eac1dc4ace1d891543b81bb
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-09-09 02:17:11 -0500

    Clients routes

---
+++ routes/clients.js
@@ -0,0 +1,54 @@
+// Load required packages
+var Logger = require('../config/logger');
+var logger = Logger.logger;
+var DataModel = require('../models/clients');
+var Client = DataModel.Client;
+// TODO: implementar mas verbos y funciones para administrar los clientes
+// ENDPOINT: /clients METHOD: GET
+exports.getClientByIdClient = function(req, res){
+    // Use the 'Client' model to find the client by his id [idUser]
+    Client.find({ idUser : req.user._id },function(err, client){
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+        }
+        // success
+        res.json(client);
+    });
+};
+
+// ENDPOINT: /clients METHOD: POST
+exports.postClient = function (req, res) {
+    // Create a new instance of the Client model
+    var client = new Client();
+
+    // Set the Client properties that came from the POST data
+    client.name = req.body.name;
+    client.id = req.body.id;
+    client.secret = req.body.secret;
+    client.idUser = req.user._id;
+
+    client.save(function(err){
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+        }
+        // success
+        res.json({ message: 'Client created successfully!', data: client });
+    });
+};
+
+// ENDPOINT: /clients/:id METHOD: DELETE
+exports.deleteClient = function(req, res){
+    Client.findByIdAndRemove(req.params.id, function(err){
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            res.send(err);
+        }
+        // success
+        res.json({ message: 'Client deleted successfully!' });
+    });
+};
