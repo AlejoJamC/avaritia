78a2dc86ce43a0d5e4dba51f5bfd683ede777760
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-09-09 02:43:03 -0500

    Routes setup modified, added 2 routes for example, oauth is ready, TODO

--- routes/routes.js
+++ routes/routes.js
@@ -26,27 +26,106 @@ function setupRouter (router){
     /**
      *  Declare all routes
      */
+    var authRoutes = require('./auth');
+    var clientRoutes = require('./clients');
+    var languageRoutes = require('./languages');
+    var oauth2Routes = require('./oauth2');
+    var serviceRoutes = require('./services');
     var userRoutes = require('./users');
+
+    /**
+     *  Document:  LANGUAGE.JS
+     *  Define routes where they are stored endpoints
+     */
+        // ENDPOINT: /languages
+        // ENDPOINT: /languages?name=value
+        // ENDPOINT: /languages?iso=value
+    router.route('/languages')
+        .get(authRoutes.isAuthenticated, languageRoutes.getLanguages)
+        .post(authRoutes.isAuthenticated, languageRoutes.postLanguage);
+
+    // ENDPOINT: /languages/:id
+    router.route('/languages/:id')
+        .get(authRoutes.isAuthenticated, languageRoutes.getLanguageById)
+        .put(authRoutes.isAuthenticated, languageRoutes.putLanguage)
+        .patch(authRoutes.isAuthenticated, languageRoutes.patchLanguage)
+        .delete(authRoutes.isAuthenticated, languageRoutes.deleteLanguage);
+    /**
+     * ====================================================================
+     */
+
+    /**
+     *  Document:  CLIENTS.JS
+     *  Define routes where they are stored endpoints
+     */
+        // ENDPOINT: /clients
+    router.route('/clients')
+        .get(authRoutes.isAuthenticated, clientRoutes.getClientByIdClient)
+        .post(authRoutes.isAuthenticated, clientRoutes.postClient);
+
+    // ENDPOINT: /clients/:id
+    router.route('/clients/:id')
+        .delete(authRoutes.isAuthenticated, clientRoutes.deleteClient);
+    /**
+     * ====================================================================
+     */
+
+    /**
+     *  Document:  OAUTH2.JS
+     *  Create endpoint handlers for oauth2 authorize
+     */
+        // ENDPOINT: /oauth2/authorize
+    router.route('/oauth2/authorize')
+        .get(authRoutes.isAuthenticated, oauth2Routes.authorization)
+        .post(authRoutes.isAuthenticated, oauth2Routes.decision);
+
+    // ENDPOINT: /oauth2/token
+    router.route('/oauth2/token')
+        .post(authRoutes.isClientAuthenticated, oauth2Routes.token);
+    /**
+     * ====================================================================
+     */
+
+    /**
+     *  Document:  SERVICES.JS
+     *  Define routes where they are stored endpoints
+     */
+        // ENDPOINT: /services
+        // ENDPOINT: /services?name=value
+    router.route('/services')
+        .get(authRoutes.isAuthenticated, serviceRoutes.getLanguages)
+        .post(authRoutes.isAuthenticated, serviceRoutes.postLanguage);
+
+    // ENDPOINT: /services/:id
+    router.route('/services/:id')
+        .get(authRoutes.isAuthenticated, serviceRoutes.getLanguageById)
+        .put(authRoutes.isAuthenticated, serviceRoutes.putLanguage)
+        .patch(authRoutes.isAuthenticated, serviceRoutes.patchLanguage)
+        .delete(authRoutes.isAuthenticated, serviceRoutes.deleteLanguage);
+    /**
+     * ====================================================================
+     */
 
     /**
      *  Document:  USERS.JS
      *  Define routes where they are stored endpoints
      */
-    //// ENDPOINT: /users
-    //router.route('/users')
-    //    .get(userRoutes.getUsers)
-    //    .post(userRoutes.postUser);
-    //
-    //// ENDPOINT: /users/:id
-    //router.route('/users/:id')
-    //    .get(userRoutes.getUserById)
-    //    .put(userRoutes.putUser)
-    //    .patch(userRoutes.patchUser)
-    //    .delete(userRoutes.deleteUser);
-    //
-    //// ENDPOINT: /login
-    //router.route('/login')
-    //    .get(userRoutes.getLogin);
+        // ENDPOINT: /users
+    router.route('/users')
+        .get(authRoutes.isAuthenticated, userRoutes.getUsers)
+        .post(userRoutes.postUser); // TODO: POST usuario no tiene seguridad
+
+    // ENDPOINT: /users/:id
+    router.route('/users/:id')
+        .get(authRoutes.isAuthenticated, userRoutes.getUserById)
+        .put(authRoutes.isAuthenticated, userRoutes.putUser)
+        .patch(authRoutes.isAuthenticated, userRoutes.patchUser)
+        .delete(authRoutes.isAuthenticated, userRoutes.deleteUser);
+
+    // ENDPOINT: /login
+    router.route('/login')
+        .get(authRoutes.isLoginAuthenticated, userRoutes.getLogin);
+
     /**
      * ====================================================================
      */
