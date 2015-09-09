c7ad3add401d054f54114b8f00cfe43857a4833c
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-09-09 01:32:14 -0500

    Language model

---
+++ models/languages.js
@@ -0,0 +1,14 @@
+// Load required packages
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+// Define our Language schema
+var LanguageSchema = new Schema({
+    name: String,
+    isoName: String,
+    creationDate: Date,
+    lastEditionDate: Date,
+    enabled: Boolean
+},{ versionKey: false });
+
+// Export the Mongoose model
+module.exports.Languages = mongoose.model('Languages', LanguageSchema);
