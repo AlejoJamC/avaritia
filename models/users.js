223a04d0fb844d540e2ffc299181c7b1e20c00e4
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-09-09 01:36:47 -0500

    User model, crypt and verify functions created

--- models/users.js
+++ models/users.js
@@ -1,3 +1,66 @@
-/**
- * Created by @AlejoJamC on 6/08/15.
- */
+// Load required packages
+var logger = require('../config/logger').logger;
+var bcrypt = require('bcrypt-nodejs');
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// TODO: Implementar validacion por correo electronico para los registros por Sign UP
+// TODO: Definir roles y permisos
+// Define basic User schema
+var UserSchema = new Schema({
+    name: String,
+    lastName: String,
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    enabled: Boolean
+},{ versionKey: false });
+
+// Execute before each user.save() call
+UserSchema.pre('save', function (callback) {
+    var user = this;
+
+    // Break out if the password hasn't changed
+    if(!user.isModified('password')) return callback();
+
+    // Password changed so we need to hash it
+    bcrypt.genSalt(5, function (err, salt) {
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            return callback(err);
+        }
+
+        bcrypt.hash(user.password, salt, null, function (err, hash) {
+            // Check for errors and show message
+            if(err){
+                logger.error(err);
+                return callback(err);
+            }
+
+            user.password = hash;
+            callback();
+        });
+    })
+});
+
+UserSchema.methods.verifyPassword = function (password, callback) {
+    bcrypt.compare(password, this.password, function (err, isMatch) {
+        // Check for errors and show message
+        if(err){
+            logger.error(err);
+            return callback(err);
+        }
+        // Make the comparation and send the answer
+        callback(null, isMatch);
+    });
+};
+
+// Export the Mongoose model
+module.exports.User = mongoose.model('User', UserSchema);
