9d5f38139b355c2f65d506469ec4eb8ed0170f1c
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-11-07 02:59:52 -0500

    added general todo

---
+++ TODO.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+// Models
+// TODO: crear un nuevo modelo para las calificaciones de los usuarios y que genere un promedio para despues acutalizar bank->score
+
+// Out of scope
