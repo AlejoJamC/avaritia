3ce39c84baf20c23582366592a27a537c9badbf5
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-11-14 23:54:48 -0500

    add license

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,11 @@
+/**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
 // Load required packages
 var logger = require('../config/logger').logger;
 var User = require('../models/users').User;
