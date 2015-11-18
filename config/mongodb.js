2a8d3b3934f3ed8e1e341bc7397f0c0e73720808
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-11-18 01:26:12 -0500

    license

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,4 +1,12 @@
 /**
+ * Copyright (c) 2015-present, Alejandro Mantilla <@AlejoJamC>.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree or translated in the assets folder.
+ */
+
+/**
  * Module dependencies
  */
 var logger = require('../config/logger').logger;
