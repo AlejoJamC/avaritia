eb2f6f6a157bf7c48763002c566767a817a4c27c
John Alejandro Mantilla Celis <alejandromantilla7@hotmail.com>
2015-11-26 23:27:25 -0500

    allowed CORS temp

--- server.js
+++ server.js
@@ -56,6 +56,14 @@ app.use(function (req, res, next) {
   next();
 });
 
+// Allow CORS
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:7000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Use the passport package in our application
 app.use(passport.initialize());
 
